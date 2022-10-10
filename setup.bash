71c984658624f8518f80bcefdda78b0908b36433
AnujDagade <dagadeanuj@gmail.com>
2022-10-10 16:42:45 +0000

    Initial setup script added

---
+++ setup.bash
@@ -0,0 +1,7 @@
+echo "Updating and Upgrading"
+sudo apt update -y && sudo apt upgrade
+echo "Updated and Upgraded"
+
+echo "Openjd-17k installing"
+sudo apt install openjdk-17-jdk
+echo "Openjd-17k installed"
